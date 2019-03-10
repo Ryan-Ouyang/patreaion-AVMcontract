42d2e912e39a0ded82751056ea39bb6782bedcdf
ryan-ouyang <ryan.ouyang01@gmail.com>
2019-03-09 21:43:34 -0500

    Added withdraw function, added balance check

--- src/main/java/patreaion/HelloAvm.java
+++ src/main/java/patreaion/HelloAvm.java
@@ -5,26 +5,61 @@ import org.aion.avm.api.BlockchainRuntime;
 
 import org.aion.avm.api.Address;
 
+import java.math.BigInteger;
+import org.aion.avm.api.Result;
+
 public class HelloAvm
 {
     private static long timeCreated;
 
     private static String _sender;
-    private static String _reciever;
+    private static Address _reciever;
     private static long interval;
     private static double amount;
-    private static double paymentsLeft;
+    private static int paymentsLeft;
 
     static {
         timeCreated = BlockchainRuntime.getBlockTimestamp();
     }
 
-    public static void start(String s, String r, long timeInterval, double cost, int totalPayments) {
+    private static void onlyOwner() {
+        BlockchainRuntime.require(BlockchainRuntime.getCaller().equals(_reciever));
+    }
+
+    public static void start(String s, long timeInterval, double cost, int totalPayments) {
         _sender = s;
-        _reciever = r;
+        _reciever = BlockchainRuntime.getOrigin();
         interval = timeInterval;
         amount = cost;
         paymentsLeft = totalPayments;
+    }
+
+    public static String withdraw() {
+        // BlockchainRuntime.println("Widthdrawing: " + withdrawal);
+        BlockchainRuntime.println("Contract Balance: " + BlockchainRuntime.getBalanceOfThisContract().longValue());
+        onlyOwner();
+        //Result result = BlockchainRuntime.call(_reciever, BigInteger.valueOf(BlockchainRuntime.getBalanceOfThisContract().longValue()), new byte[0], BlockchainRuntime.getRemainingEnergy());
+
+        Result result = null;
+        try {
+        result = BlockchainRuntime.call(_reciever, BigInteger.valueOf(BlockchainRuntime.getBalanceOfThisContract().longValue()), new byte[0], BlockchainRuntime.getRemainingEnergy());
+        } catch (Throwable t) {
+            BlockchainRuntime.println("TOTAL: " + t);
+        }
+ 
+        BlockchainRuntime.println("CAll complete: " + result);
+
+        if (result.isSuccess()) {
+        BlockchainRuntime.println("SUCCESS");
+            return ("Transfer succeeded. Remaining amount:" + BlockchainRuntime.getBalanceOfThisContract().longValue());
+        } else {
+        BlockchainRuntime.println("FAIL");
+            return ("Transfer failed.");
+        }
+    }
+
+    public static void showInfo() {
+        BlockchainRuntime.println("Remaining Balance Due: "+ BlockchainRuntime.getBalanceOfThisContract().longValue());
     }
 
     public static String sayHello() {
